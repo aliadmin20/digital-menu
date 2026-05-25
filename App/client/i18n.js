40d1bbdeb26ff670133020067ae1332834b7b21b
aliadmin20 <aliadmin20@gmail.com>
2026-05-25 17:39:20 +0300

    test

--- App/client/i18n.js
+++ App/client/i18n.js
@@ -1,5 +1,7 @@
 // postgresql://postgres.jfxrshpunxzldmqcbcgg:TtSMi1MzVbzeKutb@aws-0-eu-west-1.pooler.supabase.com:5432/postgres
 
+
+
 import { getRequestConfig } from "next-intl/server";
 import { notFound } from "next/navigation";
 
